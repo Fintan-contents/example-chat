8d3e7389af4990f686933944fbee3a739b5e2ef7
sinho-lee <i.shinho.695@tis.co.jp>
2023-11-14 15:29:15 +0900

    再確認結果、修正不要のため、既存のソースコードに戻す

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render (
-  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
