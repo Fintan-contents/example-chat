268c138973bedd0b3596a116f5dadc461804b1d6
sinho-lee <i.shinho.695@tis.co.jp>
2023-11-14 15:30:34 +0900

    ReactDOMを使用するように修正

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
-const root = createRoot(container!);
+const root = ReactDOM.createRoot(container!);
 root.render (
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
